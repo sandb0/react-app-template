a1e0f9cc5e77d8494561d8c0cc44ad593bfbcdff
sandb0 <sandbox0@hotmail.com>
2023-01-29 04:55:09 -0300

    feat: add essentials tools and configs

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
+  resolve: {
+    alias: {
+      '@modules': path.resolve(__dirname, './src/modules'),
+    },
+  },
+});
